4d20e7f83f3a66d02ca4fd125b88f58e7b10a8d3
danielrebelo1 <daniel.rebelo2003@gmail.com>
2023-03-07 10:47:17 +0000

    lab2 done

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+extern int counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -49,8 +50,37 @@ int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
 }
 
 int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  
+  uint8_t bit_no;
+  timer_subscribe_int(&bit_no);
+  uint32_t irq_set = BIT(bit_no);
+  int ipc_status;
+  message msg;
+  while( 1 ) { /* You may want to use a different condition */
+      /* Get a request message. */
+      int r;
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+      printf("driver_receive failed with: %d", r);
+      continue;
+}
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */				
+          if (msg.m_notify.interrupts & irq_set) {
+            timer_int_handler();
+            
+            if (counter % 60 == 0){
+              timer_print_elapsed_time();
+              time--;
+            } /* subscribed interrupt */ /* process it */
+        }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */	
+      }
+    } else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+    }
+ }
+  return 0;
 }
--- lab2/timer.c
+++ lab2/timer.c
@@ -5,7 +5,7 @@
 
 #include "i8254.h"
 
-static uint8_t hook_id = 1;
+static int hook_id = 1;
 int counter = 0;
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
@@ -62,7 +62,7 @@ int (timer_subscribe_int)(uint8_t *bit_no) {
 }
 
 int (timer_unsubscribe_int)() {
-  if (sys_irqrmpolicy(hook_id) != 0) return 1;
+  if (sys_irqrmpolicy(&hook_id) != 0) return 1;
   return 1;
 }
 
